c0ffda877f9fcf614c81c595291bade4e035f0f5
Sujin Lee <sujinhhh@hotmail.com>
2021-09-03 09:43:41 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,78 +1,32 @@
-// function solution(str, ending) {
-//   // TODO: complete
-//   let a = str.endsWith(ending);
-//   a;
-// }
-// console.log(solution("abc", "bc"));
-// console.log(str.endsWith("to be", 19));
-// // 19글자로 잘라서 끝부분 확인
+const numIslands=(grid)=> {
+  let countIsland = 0;
 
-// function solution(str, ending) {
-//   return new RegExp(ending + "$", "i").test(str);
-// }
+  const changeToWater = (rowIn, colIn) => {
+    if (
+      grid[rowIn] === undefined ||
+      grid[rowIn][colIn] === undefined ||
+      grid[rowIn][colIn] === "0"
+    )
+      return;
+    grid[rowIn][colIn] = "0";
+    numIslands(rowIn + 1, colIn);
+    numIslands(rowIn - 1, colIn);
+    numIslands(rowIn, colIn + 1);
+    numIslands(rowIn, colIn - 1);
+   
+  };
 
-// function solution(str, ending) {
-//   return str.substr(-ending.length) == ending;
-// }
-
-// // 첫글자 대문자 만들기
-
-// String.prototype.toJadenCase = function () {
-//     return this.split(" ").map(function(word){
-//       return word.charAt(0).toUpperCase() + word.slice(1);
-//     }).join(" ");
-//   }
-var array1 = [
-  true,
-  true,
-  true,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  true,
-  false,
-  true,
-  false,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  true,
-  true,
-];
-function countSheeps(arrayOfSheep) {
-  let number = 0;
-  for (let i of arrayOfSheep) {
-    i == true ? (number += 1) : (number += 0);
-  }
-  console.log(number);
-}
-
-countSheeps(array1);
-
-// 위에 것 간단하게
-function countSheeps(arrayOfSheeps) {
-  return arrayOfSheeps.filter(Boolean).length;
-}
-
-function func(range) {
-  m = {};
-
-  for (let i in range) {
-    console.log(c);
-
-    m[c] = i;
+  for (let rowIndex in grid) {
+    for (let colIndex in grid[rowIndex]) {
+      if (grid[rowIndex][colIndex] === "1") {
+        countIsland++;
+        changeToWater(parseInt(rowIndex), parseInt(colIndex));
+      }
+    }
   }
 
-  return m;
+  console.log(countIsland);
+  return countIsland;
 }
 
-func(5);
+numIslands(gridA);
